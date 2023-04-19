55dea7da83e7666f839a8f7692727e9597a8a83f
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-04-18 20:52:07 -0500

    added routes and changes to server.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+router.get("/notes", (req, res) => {
+    res.send('Hello World');
+})
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+const fs = require('fs');
+const path = require('path');
+const db = require('./db/db.json');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
-app.listen(PORT, function () { console.log(`Express server listening on port: ${PORT}`);});
+app.listen(PORT, function() {console.log(`Express server listening on port: ${PORT}`);});

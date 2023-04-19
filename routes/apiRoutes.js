d3fc4040aeddf2c2ddbde848f97dd01a2dfde41f
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-04-18 21:00:00 -0500

    add new route htmlRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,3 +3,5 @@ const router = require('express').Router();
 router.get("/notes", (req, res) => {
     res.send('Hello World');
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const db = require('./db/db.json');
+
+const apiRoutes = require('./routes/apiRoutes')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
 
 app.listen(PORT, function() {console.log(`Express server listening on port: ${PORT}`);});

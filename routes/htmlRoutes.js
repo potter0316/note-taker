ea07c709653011ccb24e051704621d3b77be61f1
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-04-20 18:50:50 -0500

    changes to htmlRoutes and server.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,5 +2,13 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    console.info(`${req.method} Received ${req.path}`)
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+    console.info(`${req.method} Recieved ${req.path}`)
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, function () {
   console.log(`Express server listening on port: ${PORT}`);

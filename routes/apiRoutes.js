ffa0b4daa199ff4be83281f084577f1dd34bf8e7
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-04-20 19:48:09 -0500

    getting notes from post request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
 const database = require("../db/db.json");
+const fs = require("fs");
+const path = require("path");
+
 router.get("/notes", (req, res) => {
-  console.info(`Getting notes from ${database}`);
+  console.info(`Getting notes from the database...`);
   res.json(database);
 });
 
+router.post("/notes", (req, res) => {
+    let jsonPath = path.join(__dirname, "/db/db.json");
+    console.ing(req.body);
+    database.push(newNote)
+    fs.writeFile(jsonPath, JSON.stringify(database), function (err) { (err) ? console.log(err) : console.log("Success!") });
+    res.json(req.body);
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const db = require("./db/db.json");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
-app.use('/', htmlRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, function () {
   console.log(`Express server listening on port: ${PORT}`);

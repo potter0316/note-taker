a34dc6c1713c93f0394f50fc78c32a1f6a5787cf
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-04-24 18:53:51 -0500

    added more to delete on apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,45 @@
 const router = require("express").Router();
-const database = require("../db/db.json");
+const db = require("../db/db.json");
 const fs = require("fs");
 const path = require("path");
+const uuid = require("uuid");
 
 router.get("/notes", (req, res) => {
   console.info(`Getting notes from the database...`);
-  res.json(database);
+  res.json(db);
+});
+
+router.delete("/api/notes/:id", (req, res) => {
+  res.json(req);
+});
+
+router.delete("/api/notes/:id", (req, res) => {
+    for (i = 0; i < db.length; i++) {
+        if (db[i].id === req.params.id) {
+            db.splice(i, 1);
+            break;
+        }
+        else {
+            return "Error"
+        }
+    }
+    console.info(`Delete note ${req.body}`);
+    res.json(req.body.title);
 });
 
 router.post("/notes", (req, res) => {
-    let jsonPath = path.join(__dirname, "/db/db.json");
-    console.ing(req.body);
-    database.push(newNote)
-    fs.writeFile(jsonPath, JSON.stringify(database), function (err) { (err) ? console.log(err) : console.log("Success!") });
-    res.json(req.body);
-})
+  let jsonPath = path.join(__dirname, "../db/db.json");
+    note.id = uuid.v4();
+    console.log(`Note ID: ${note.id}`);
+  console.info(`New note: ${note.tile} is saved to the database with an id of ${note.id}`
+  );
+  db.push(note);
+
+  fs.writeFile(jsonPath, JSON.stringify(db), function (err) {
+    err ? console.log(err) : console.log("Success");
+  });
+
+  res.json(note);
+});
+
 module.exports = router;

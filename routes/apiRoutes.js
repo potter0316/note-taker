6c672126afce12be3e479a81adae01d20bdc37de
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-04-24 19:59:42 -0500

    delete notes and update json file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,25 +13,30 @@ router.delete("/api/notes/:id", (req, res) => {
   res.json(req);
 });
 
-router.delete("/api/notes/:id", (req, res) => {
-    for (i = 0; i < db.length; i++) {
-        if (db[i].id === req.params.id) {
-            db.splice(i, 1);
-            break;
-        }
-        else {
-            return "Error"
-        }
+router.delete("/notes/:id", (req, res) => {
+    let jsonPath = path.join(__dirname, "../db/db.json");
+  for (i = 0; i < db.length; i++) {
+    if (db[i].id == req.params.id) {
+      db.splice(i, 1);
+      break;
+    } else {
+      return "Error";
     }
-    console.info(`Delete note ${req.body}`);
-    res.json(req.body.title);
+  }
+  fs.writeFileSync(jsonPath, JSON.stringify(db), (err) => {
+    console.info(`Opps Something Went Wrong with ID: ${req.params.id}`);
+    console.error(err);
+  });
+  console.info(`Delete note ${req.body}`);
+  res.json(db);
 });
 
 router.post("/notes", (req, res) => {
   let jsonPath = path.join(__dirname, "../db/db.json");
-    note.id = uuid.v4();
-    console.log(`Note ID: ${note.id}`);
-  console.info(`New note: ${note.tile} is saved to the database with an id of ${note.id}`
+  note.id = uuid.v4();
+  console.log(`Note ID: ${note.id}`);
+  console.info(
+    `New note: ${note.title} is saved to the database with an id of ${note.id}`
   );
   db.push(note);
 
